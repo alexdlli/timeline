aa6f838faba069b017d4f50e12332e3b279e018e
alexdlli <alexsandrojr.303@gmail.com>
2025-08-08 16:28:47 -0300

    chore: simplify Layout component by removing unnecessary styles

--- src/presentation/layouts/layout.tsx
+++ src/presentation/layouts/layout.tsx
@@ -1,20 +1,5 @@
-import { css } from '@emotion/react';
 import type { PropsWithChildren } from 'react';
 
 export function Layout({ children }: PropsWithChildren) {
-  return (
-    <main
-      css={css`
-      justify-items: center;
-      align-items: center;
-      justify-content: center;
-      padding: 16px;
-
-      margin: 0 auto;
-      background-color: transparent;
-      `}
-    >
-      {children}
-    </main>
-  );
+  return <main>{children}</main>;
 }
